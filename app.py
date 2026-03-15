0291bbc1b3dc56c52046679ef679258d7fe858e5
mugdhachalla <mugdha.challa@gmail.com>
2026-03-15 13:14:04 +0530

    deploying

--- app.py
+++ app.py
@@ -307,5 +307,8 @@ def pitch():
     return render_template("results.html", pitch=pitch)
 
 
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
