119f89e40d88115c79a02b5c8c921c70ff9a7296
mugdhachalla <mugdha.challa@gmail.com>
2026-03-19 07:41:27 +0530

    prpeare for deployment:

--- app.py
+++ app.py
@@ -309,6 +309,6 @@ def pitch():
 
 
 
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
