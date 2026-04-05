4862bc16e87d319c0fdce90c837629728d91bff0
mugdhachalla <mugdha.challa@gmail.com>
2026-04-05 10:49:22 +0530

    fixing deployment

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-app = Flask(__name__)
+from huggingface_hub import InferenceClient
 from flask import Flask
-
+import os
 
 app = Flask(__name__)
 
@@ -8,3 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "App is running"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
